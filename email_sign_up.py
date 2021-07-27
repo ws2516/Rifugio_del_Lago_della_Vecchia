0a75d8fafea7e3dfd151a4aae6bfe7294a5bdb06
William Shamma <william.shamma2@gmail.com>
2021-07-27 02:00:32 +0200

    working MVP without google sheet funtionality though forms are in place - for that email_update.py must be tweaked

--- email_sign_up.py
+++ email_sign_up.py
@@ -17,10 +17,3 @@ def write_to_sheet(username, bet, odds, units, sport, league, record, roi, net_u
 def next_available_row(worksheet):
     str_list = list(filter(None, worksheet.col_values(1)))
     return str(len(str_list)-2)
-
-def format_correctly(bet, odds, units, sport, league, record, roi, net_units, message):
-    string = '<b>Record</b>: ' + record + ' <br><br>' + '<b>ROI</b>: ' + roi + '<br><br>' + '<b>Net_units</b>: ' + net_units + '<br><br>'
-    string2 = '| ' + sport + ' | ' + league + ' |<br><br>'
-    string3 = '<b>Pick</b>: ' + bet + ' - ' + '<b>Odds</b>: ' + odds + ' - ' + '<b>Units</b>: ' + units + '<br><br>'
-    string4 = '<b>Write Up</b>: ' + message
-    return string + string2 + string3 + string4
