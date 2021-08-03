a4351ad10e07c70edf599ff60f41ad6d9cd400e3
William Shamma <william.shamma2@gmail.com>
2021-08-03 20:19:12 +0200

    preload font

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 import gunicorn
+import email_sign_up
 
 from flask import Flask, request, render_template, session, redirect
 from flask import send_from_directory
@@ -12,6 +13,14 @@ def main():
     if flask.request.method == 'GET':
 
         return flask.render_template('index.html')
+    
+    if flask.request.method == 'POST':
+    	
+    	email = flask.request.form['Email']
+    	
+    	thank_you = email_sign_up.write_to_sheet(email)
+
+        return flask.render_template('index.html', thanks = thank_you)
 
 
 if __name__ == '__main__':
--- email_sign_up.py
+++ email_sign_up.py
@@ -4,15 +4,14 @@ import datetime
 from datetime import datetime
 import os
 
-def write_to_sheet(username, bet, odds, units, sport, league, record, roi, net_units, message):
+def write_to_sheet(name): #, email, telephone, quantity, arrival, departure, message):
 	credentials = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
 	creds = ServiceAccountCredentials.from_json_keyfile_name(credentials)
 	client = gspread.authorize(creds)
-	sheet = client.open("POTDTracker").worksheet('Week1')
-	row = [username, bet, odds, units, sport, league, record, roi, net_units, message, str(datetime.today())]
+	sheet = client.open("Prenotare").sheet1
+	row = [name] #, email, telephone, quantity, arrival, departure, message, str(datetime.today())]
 	sheet.append_row(row)
-	formatted = format_correctly(bet, odds, units, sport, league, record, roi, net_units, message)
-	return formatted
+	return 'Thank you, we have received your request!'
 
 def next_available_row(worksheet):
     str_list = list(filter(None, worksheet.col_values(1)))
